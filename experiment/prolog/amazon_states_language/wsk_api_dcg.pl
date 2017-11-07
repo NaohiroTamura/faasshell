4ad452f4aee6f3f5eded884d7ba6a2906872bc6e
Naohiro Tamura <naohirot@jp.fujitsu.com>
2017-11-07 13:47:09 +0000

    fixed the grammar so that phrase() can work reverse direction

--- experiment/prolog/amazon_states_language/wsk_api_dcg.pl
+++ experiment/prolog/amazon_states_language/wsk_api_dcg.pl
@@ -7,23 +7,18 @@
 
 %% OpenWhisk REST API Grammar
 url(get)     --> verb(get), namespaces.
-
 url(get,N)   --> verb(get), namespaces, namespace(N).
-
 url(get,N,A) --> verb(get), namespaces, namespace(N), apis(A).
-
-url(V,N,A,E) --> verb(V), namespaces, namespace(N), apis(A), entity(E).
-
+url(V,N,A,E) --> verb(V),   namespaces, namespace(N), apis(A), entity(E).
 %% Actions
-url(get,N,actions,E,Q)  --> url(get,N,actions,E), ["?"], query(get, Q).
+url(get,N,actions,E,Q)  --> url(get,N,actions,E),  ["?"], query(get, Q).
 url(post,N,actions,E,Q) --> url(post,N,actions,E), ["?"], query(post, Q).
-
 %% Activations
-url(get,N,activations) --> url(get,N,activations), ["?"], query(Q).
+url(get,N,activations,Q)   --> url(get,N,activations), ["?"], query(get, Q).
 url(get,N,activations,E,R) --> url(get,N,activations,E), resource(R).
 
 %%
-query(V, [Q])    --> param(V, Q), {!}.
+query(V, [Q])    --> param(V, Q), !.
 query(V, [Q|Qs]) --> param(V, Q), ["&"], query(V, Qs).
 
 verb(get)     --> ["get"].
@@ -33,8 +28,7 @@ verb(post)    --> ["post"].
 verb(patch)   --> { fail }.
 
 namespaces    --> ["/", "namespaces"].
-
-namespace(N)  --> { atom_string(N, S) }, ["/", S].
+namespace(N)  --> ["/", S], { atom_string(N, S) }.
 
 apis(actions)     --> ["/", "actions"].
 apis(rules)       --> ["/", "rules"].
@@ -42,16 +36,16 @@ apis(triggers)    --> ["/", "triggers"].
 apis(activations) --> ["/", "activations"].
 apis(packages)    --> ["/", "packages"].
 
-entity(E)    --> { atom_string(E, S) }, ["/", S].
+entity(E)    --> ["/", S], { atom_string(E, S) }.
 
-param(get, limit=N)     --> { number_string(N,S) }, ["limit", "=", S].
-param(get, skip=N)      --> { number_string(N,S) }, ["skip",  "=", S].
-param(get, since=N)     --> { number_string(N,S) }, ["since", "=", S].
-param(get, upto=N)      --> { number_string(N,S) }, ["upto",  "=", S].
-param(get, docs=B)      --> { atom_string(B,S)   }, ["docs",  "=", S].
-param(post, blocking=B) --> { atom_string(B,S)   }, ["blocking", "=", S].
-param(post, result=B)   --> { atom_string(B,S)   }, ["result", "=", S].
-param(post, timeout=N)  --> { number_string(N,S) }, ["timeout", "=", S].
+param(get, limit=N)     --> ["limit", "=", S], { number_string(N,S) }.
+param(get, skip=N)      --> ["skip", "=", S], { number_string(N,S) }.
+param(get, since=N)     --> ["since", "=", S], { number_string(N,S) }.
+param(get, upto=N)      --> ["upto", "=", S], { number_string(N,S) }.
+param(get, docs=B)      --> ["docs", "=", S], { atom_string(B,S) }.
+param(post, blocking=B) --> ["blocking", "=", S], { atom_string(B,S) }.
+param(post, result=B)   --> ["result", "=", S], { atom_string(B,S)  }.
+param(post, timeout=N)  --> ["timeout", "=", S], { number_string(N,S) }.
 
 resource(logs)   --> ["/", "logs"].
 resource(result) --> ["/", "result"].
@@ -65,18 +59,29 @@ resource(result) --> ["/", "result"].
 :- begin_tests(actions).
 
 test(get, Xs == ["get", "/", "namespaces", "/", "guest", "/", "actions"]) :-
-    phrase(url(get, guest, actions), Xs, []).
+    phrase(url(get, guest, actions), Xs).
+
+test(get_rev, (V, N, A) == (get, guest, actions)) :-
+    phrase(url(V, N, A), ["get", "/", "namespaces", "/", "guest", "/", "actions"]).
 
 test(illegal_verb, fail) :-
-    phrase(url(put, guest, actions), _Xs, []).
+    phrase(url(put, guest, actions), _Xs).
 
 test(action_name, Xs == ["get", "/", "namespaces", "/", "guest", "/", "actions",
                          "/", "hello"]) :-
-    phrase(url(get, guest, actions, hello), Xs, []).
+    phrase(url(get, guest, actions, hello), Xs).
+
+test(action_name_rev, (V, N, A, E) == (get, guest, actions, hello)) :-
+    phrase(url(V, N, A, E),
+           ["get", "/", "namespaces", "/", "guest", "/", "actions", "/", "hello"]).
 
 test(query, Xs == ["get", "/", "namespaces", "/", "guest", "/", "actions",
                    "/", "hello", "?", "limit", "=", "1"]) :-
     url(get, guest, actions, hello, [limit=1], Xs, []).
+
+test(query_rev, (V, N, A, E, Q)  == (get, guest, actions, hello, [limit=1])) :-
+    url(V, N, A, E, Q, ["get", "/", "namespaces", "/", "guest", "/", "actions",
+                        "/", "hello", "?", "limit", "=", "1"], []).
 
 test(query, Xs == ["get", "/", "namespaces", "/", "guest", "/", "actions",
                    "/", "hello", "?", "skip", "=", "2"]) :-
@@ -91,44 +96,44 @@ test(unknown_query, fail) :-
 
 test(put, Xs == ["put", "/", "namespaces", "/", "guest", "/", "actions",
                  "/", "hello"]) :-
-    phrase(url(put, guest, actions, hello), Xs, []).
+    phrase(url(put, guest, actions, hello), Xs).
 
 test(delete, Xs == ["delete", "/", "namespaces", "/", "guest", "/", "actions",
                     "/", "hello"]) :-
-    phrase(url(delete, guest, actions, hello), Xs, []).
+    phrase(url(delete, guest, actions, hello), Xs).
 
 test(post, Xs == ["post", "/", "namespaces", "/", "guest", "/", "actions",
                   "/", "hello"]) :-
-    phrase(url(post, guest, actions, hello), Xs, []).
+    phrase(url(post, guest, actions, hello), Xs).
 
 test(query, Xs == ["post", "/", "namespaces", "/", "guest", "/", "actions",
                   "/", "hello", "?", "blocking", "=", "true"]) :-
-    phrase(url(post, guest, actions, hello, [blocking=true]), Xs, []).
+    phrase(url(post, guest, actions, hello, [blocking=true]), Xs]).
 
 test(query, Xs == ["post", "/", "namespaces", "/", "guest", "/", "actions",
                   "/", "hello", "?", "result", "=", "true"]) :-
-    phrase(url(post, guest, actions, hello, [result=true]), Xs, []).
+    phrase(url(post, guest, actions, hello, [result=true]), Xs).
 
 test(query, Xs == ["post", "/", "namespaces", "/", "guest", "/", "actions",
                   "/", "hello", "?", "timeout", "=", "600"]) :-
-    phrase(url(post, guest, actions, hello, [timeout=600]), Xs, []).
+    phrase(url(post, guest, actions, hello, [timeout=600]), Xs).
 
 test(query, Xs == ["post", "/", "namespaces", "/", "guest", "/", "actions",
                    "/", "hello", "?", "blocking", "=", "true", "&", "result",
                    "=", "true"]) :-
-    phrase(url(post, guest, actions, hello, [blocking=true, result=true]), Xs, []).
+    phrase(url(post, guest, actions, hello, [blocking=true, result=true]), Xs).
 
 test(query, Xs == ["post", "/", "namespaces", "/", "guest", "/", "actions",
                    "/", "hello", "?", "blocking", "=", "true", "&", "result",
                    "=", "true", "&", "timeout", "=", "600"]) :-
     phrase(url(post, guest, actions, hello,
-               [blocking=true, result=true, timeout=600]), Xs, []).
+               [blocking=true, result=true, timeout=600]), Xs).
 
 test(illegal_query, fail) :-
-    phrase(url(post, guest, actions, hello, [limit=1]), _Xs, []).
+    phrase(url(post, guest, actions, hello, [limit=1]), _Xs).
 
 test(patch, fail) :-
-    phrase(url(patch, guest, actions), _Xs, []).
+    phrase(url(patch, guest, actions), _Xs).
 
 :- end_tests(actions).
 %%
@@ -137,26 +142,26 @@ test(patch, fail) :-
 :- begin_tests(rules).
 
 test(get, Xs == ["get", "/", "namespaces", "/", "guest", "/", "rules"]) :-
-    phrase(url(get, guest, rules), Xs, []).
+    phrase(url(get, guest, rules), Xs).
 
 test(illegal_verb, fail) :-
-    phrase(url(put, guest, rules), _Xs, []).
+    phrase(url(put, guest, rules), _Xs).
 
 test(package_name, Xs == ["get", "/", "namespaces", "/", "guest", "/",
                           "rules", "/", "hello"]) :-
-    phrase(url(get, guest, rules, hello), Xs, []).
+    phrase(url(get, guest, rules, hello), Xs).
 
 test(put, Xs == ["put", "/", "namespaces", "/", "guest", "/", "rules", "/",
                  "hello"]) :-
-    phrase(url(put, guest, rules, hello), Xs, []).
+    phrase(url(put, guest, rules, hello), Xs).
 
 test(delete, Xs == ["delete", "/", "namespaces", "/", "guest", "/", "rules",
                     "/", "hello"]) :-
-    phrase(url(delete, guest, rules, hello), Xs, []).
+    phrase(url(delete, guest, rules, hello), Xs).
 
 test(post, Xs == ["post", "/", "namespaces", "/", "guest", "/", "rules",
                   "/", "hello"]) :-
-    phrase(url(post, guest, rules, hello), Xs, []).
+    phrase(url(post, guest, rules, hello), Xs).
 
 :- end_tests(rules).
 %%
@@ -165,26 +170,26 @@ test(post, Xs == ["post", "/", "namespaces", "/", "guest", "/", "rules",
 :- begin_tests(triggers).
 
 test(get, Xs == ["get", "/", "namespaces", "/", "guest", "/", "triggers"]) :-
-    phrase(url(get, guest, triggers), Xs, []).
+    phrase(url(get, guest, triggers), Xs).
 
 test(illegal_verb, fail) :-
-    phrase(url(put, guest, triggers), _Xs, []).
+    phrase(url(put, guest, triggers), _Xs).
 
 test(package_name, Xs == ["get", "/", "namespaces", "/", "guest", "/",
                           "triggers", "/", "hello"]) :-
-    phrase(url(get, guest, triggers, hello), Xs, []).
+    phrase(url(get, guest, triggers, hello), Xs).
 
 test(put, Xs == ["put", "/", "namespaces", "/", "guest", "/", "triggers", "/",
                  "hello"]) :-
-    phrase(url(put, guest, triggers, hello), Xs, []).
+    phrase(url(put, guest, triggers, hello), Xs).
 
 test(delete, Xs == ["delete", "/", "namespaces", "/", "guest", "/", "triggers",
                     "/", "hello"]) :-
-    phrase(url(delete, guest, triggers, hello), Xs, []).
+    phrase(url(delete, guest, triggers, hello), Xs).
 
 test(post, Xs == ["post", "/", "namespaces", "/", "guest", "/", "triggers",
                   "/", "hello"]) :-
-    phrase(url(post, guest, triggers, hello), Xs, []).
+    phrase(url(post, guest, triggers, hello), Xs).
 
 :- end_tests(triggers).
 %%
@@ -193,25 +198,25 @@ test(post, Xs == ["post", "/", "namespaces", "/", "guest", "/", "triggers",
 :- begin_tests(activations).
 
 test(get, Xs == ["get", "/", "namespaces", "/", "guest", "/", "activations"]) :-
-    phrase(url(get, guest, activations), Xs, []).
+    phrase(url(get, guest, activations), Xs).
 
 test(illegal_verb, fail) :-
-    phrase(url(put, guest, activations), _Xs, []).
+    phrase(url(put, guest, activations), _Xs).
 
 test(activationid, Xs == ["get", "/", "namespaces", "/", "guest", "/",
                           "activations", "/", "101"]) :-
-    phrase(url(get, guest, activations, 101), Xs, []).
+    phrase(url(get, guest, activations, 101), Xs).
 
 test(resource, Xs == ["get", "/", "namespaces", "/", "guest", "/",
                       "activations", "/", "101", "/", "logs"]) :-
-    phrase(url(get, guest, activations, 101, logs), Xs, []).
+    phrase(url(get, guest, activations, 101, logs), Xs).
 
 test(resource, Xs == ["get", "/", "namespaces", "/", "guest", "/",
                       "activations", "/", "101", "/", "result"]) :-
-    phrase(url(get, guest, activations, 101, result), Xs, []).
+    phrase(url(get, guest, activations, 101, result), Xs).
 
 test(unknown_resource, fail) :-
-    phrase(url(get, guest, activations, 101, unknown), _Xs, []).
+    phrase(url(get, guest, activations, 101, unknown), _Xs).
 
 :- end_tests(activations).
 %%
@@ -220,22 +225,22 @@ test(unknown_resource, fail) :-
 :- begin_tests(packages).
 
 test(get, Xs == ["get", "/", "namespaces", "/", "guest", "/", "packages"]) :-
-    phrase(url(get, guest, packages), Xs, []).
+    phrase(url(get, guest, packages), Xs).
 
 test(illegal_verb, fail) :-
-    phrase(url(put, guest, packages), _Xs, []).
+    phrase(url(put, guest, packages), _Xs).
 
 test(package_name, Xs == ["get", "/", "namespaces", "/", "guest", "/",
                           "packages", "/", "hello"]) :-
-    phrase(url(get, guest, packages, hello), Xs, []).
+    phrase(url(get, guest, packages, hello), Xs).
 
 test(put, Xs == ["put", "/", "namespaces", "/", "guest", "/", "packages", "/",
                  "hello"]) :-
-    phrase(url(put, guest, packages, hello), Xs, []).
+    phrase(url(put, guest, packages, hello), Xs).
 
 test(delete, Xs == ["delete", "/", "namespaces", "/", "guest", "/", "packages",
                     "/", "hello"]) :-
-    phrase(url(delete, guest, packages, hello), Xs, []).
+    phrase(url(delete, guest, packages, hello), Xs).
 
 :- end_tests(packages).
 
@@ -243,13 +248,13 @@ test(delete, Xs == ["delete", "/", "namespaces", "/", "guest", "/", "packages",
 :- begin_tests(namespaces).
 
 test(get, Xs == ["get", "/", "namespaces"]) :-
-    phrase(url(get), Xs, []).
+    phrase(url(get), Xs).
 
 test(illegal_verb, fail) :-
-    phrase(url(put), _Xs, []).
+    phrase(url(put), _Xs).
 
 test(get, Xs == ["get", "/", "namespaces", "/", "guest"]) :-
-    phrase(url(get, guest), Xs, []).
+    phrase(url(get, guest), Xs).
 
 :- end_tests(namespaces).
 %%
